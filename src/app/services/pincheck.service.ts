d91bae58c2a634cd76150c17fefad4b34287a495
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2019-05-25 17:34:59 +0800

    Temporary faas record search; PIN Checker

---
+++ src/app/services/pincheck.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class pincheck {
+  constructor(private http: HttpClient) { }
+
+  checkPin(data: any): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + localStorage.getItem('auth')
+    });
+    let opt = { headers: headers };
+    return this.http.post('http://192.168.100.24:5000/api/check-pin-land', data, opt);
+  }
+}
