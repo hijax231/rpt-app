fdce297a69ba4d642c110b42c2f5aad49c2b3f52
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2019-06-01 06:16:06 +0800

    Added docx generation; ToDo: Open file on browser and print

--- src/app/services/genLandTaxCl.ts
+++ src/app/services/genLandTaxCl.ts
@@ -1,15 +1,37 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-
-
+import docxtemplater from 'docxtemplater';
+import * as JSZip from 'jszip';
+import * as JSZipUtils from 'jszip-utils';
+import { saveAs } from 'file-saver'; 
 @Injectable({
   providedIn: 'root'
 })
 
 export class genLandTaxCl {
 
+  URL: string = '../assets/temp/clearance_template.docx';
+
+  loadFile(url: any, callback: any) {
+    JSZipUtils.getBinaryContent(url, callback);
+  }
 
   lTaxCl(data: any): void {
-    console.log(data)
+    this.loadFile(this.URL, (err: any, cont: any) => {
+      if(err) { throw err; }
+      const zip = new JSZip(cont);
+      const doc = new docxtemplater().loadZip(zip)
+      doc.setData(data)
+      try {
+        doc.render()
+      } catch (e) {
+        console.log(JSON.stringify({ error: e }))
+        throw e;
+      }
+      const out = doc.getZip().generate({
+        type: 'blob',
+        mimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+      })
+      saveAs(out, 'output.docx');
+    })
   }
 }
